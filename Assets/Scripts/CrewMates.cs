3ffcc267481240fbb126fa654bf53f6c04025758
ConaLuk <101550705+ConaLuk@users.noreply.github.com>
2022-10-19 14:47:59 +1100

    started script
    only declared first 2 values

--- Assets/Scripts/CrewMates.cs
+++ Assets/Scripts/CrewMates.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+namespace ConnorLuck 
+{ 
+
+
 public class CrewMates : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        [Header("Application")]
+        private string crewName;
+        private string crewHobby;
+
+        private void Start()
+        {
+            
+        }
+
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
