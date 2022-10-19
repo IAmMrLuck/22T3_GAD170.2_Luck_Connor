1b1fe378775fb0a75687ee6bbcb5ce71dac85c00
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2022-10-19 19:23:41 +1100

    a little bit more

--- Assets/Scripts/CrewMates.cs
+++ Assets/Scripts/CrewMates.cs
@@ -9,12 +9,17 @@ namespace ConnorLuck
 public class CrewMates : MonoBehaviour
 {
         [Header("Application")]
+        [SerializeField] public 
         private string crewName;
         private string crewHobby;
+        private bool parasite = false;
 
         private void Start()
         {
-            
+            switch (switch_on)
+            {
+                default:
+            }
         }
 
 
