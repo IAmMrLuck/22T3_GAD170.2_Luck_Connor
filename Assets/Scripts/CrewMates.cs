4136219a2727df7811504fb947e5395ddbb99fbf
ConaLuk <101550705+ConaLuk@users.noreply.github.com>
2022-11-09 09:39:40 +1100

    list changes

--- Assets/Scripts/CrewMates.cs
+++ Assets/Scripts/CrewMates.cs
@@ -1,25 +1,44 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 namespace ConnorLuck 
 { 
-
+    // I have elected not to remove any working which did not benefit my scripts - for the purposes of growth and learning
 
     public class CrewMates : MonoBehaviour
     {
-        private string[] crewMatesArray = new string[10] { "Brock", "Misty", "Lt.Surge", "Erika", "Koga", "Sabrina", "Blaine", "Giovanni", "Red", "Blue" };
+        public void CallNextCrewMate(string newName, string newHobby, int newPrice)
+        {
+            name = newName;
+            hobby = newHobby;
+           
+        }
+
+        // creating a list of names for the game by using an array and a list
+        // this is not needed 
+        // string[] crewMatesArray = new string[10] { "Brock", "Misty", "Lt.Surge", "Erika", "Koga", "Sabrina", "Blaine", "Giovanni", "Red", "Blue" };
+
 
         private List<string> crewMatesList = new List<string>() { "Brock", "Misty", "Lt.Surge", "Erika", "Koga", "Sabrina", "Blaine", "Giovanni", "Red", "Blue" };
+
+        //creating a list of names for the game by using the same as above - I will need to do this for the realHobbies and the fakeHobbies
+        private string[] realHobbiesArray = new string[10] { "Bouldering", "Swimming", "Electrical", "Gardening", "Ninjas", "Magic Tricks", "Fire Breathing", "Organised Crime", "Being the Best", "Being the Worst" };
+
+        //do not need both an array and a list - instantiating 
+        //private List<string> realHobbiesList = new List<string>() { "Bouldering", "Swimming", "Electrical", "Gardening", "Ninjas", "Magic Tricks", "Fire Breathing", "Organised Crime", "Being the Best", "Being the Worst" };
+
+     
 
         private void Start()
         {
             //foreach string in crewMatesarray call random name
-            foreach (var item in crewMatesArray)
+            foreach (string item in crewMatesList)
             {
 
-                Console.WriteLine(crewMatesArray);
+                Console.WriteLine(crewMatesList);
                 
             }
               
